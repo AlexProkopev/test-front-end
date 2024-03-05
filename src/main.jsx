1f0d38dccea8d7cba6abab00782dd8f95f41e0aa
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2024-03-05 21:50:10 +0100

    de

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,14 @@ import App from './App.jsx';
 import './index.css';
 
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/test-front-end">
     <Provider store={store}>
-     
       <App />
-
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
